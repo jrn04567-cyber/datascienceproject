59d492a3c279d1455f8d464bc8c2698e80b44ad3
Jyoti <jrn.04567@gmail.com>
2026-09-19 21:02:44 +0530

    added new model trainer files

--- src/datascience/config/configuration.py
+++ src/datascience/config/configuration.py
@@ -1,6 +1,6 @@
 from datascience.constants import *
 from datascience.utils.common import read_yaml, create_directories
-from datascience.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from datascience.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -56,3 +56,41 @@ class ConfigurationManager:
             )
     
             return data_transformation_config
+
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+     config = self.config.model_trainer
+     params = self.params.ElasticNet
+     schema = self.schema.TARGET_COLUMN
+
+     create_directories([config.root_dir])
+
+     model_trainer_config = ModelTrainerConfig(
+         root_dir=config.root_dir,
+         train_data_path=config.train_data_path,
+         test_data_path=config.test_data_path,
+         model_name=config.model_name,
+         alpha=params.alpha,
+         l1_ratio=params.l1_ratio,
+         target_column=schema.name
+     )
+
+     return model_trainer_config
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+     config = self.config.model_evaluation
+     params = self.params.ElasticNet
+     schema = self.schema.TARGET_COLUMN
+
+     create_directories([config.root_dir])
+
+     model_evaluation_config = ModelEvaluationConfig(
+         root_dir=config.root_dir,
+         test_data_path=config.test_data_path,
+         model_path=config.model_path,
+         all_params=params,
+         metric_file_name=config.metric_file_name,
+         target_column=schema.name,
+         mlflow_uri="https://dagshub.com/<YOUR_DAGSHUB_USERNAME>/<YOUR_DAGSHUB_REPO>.mlflow"
+     )
+
+     return model_evaluation_config
